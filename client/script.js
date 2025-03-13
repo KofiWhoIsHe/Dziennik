908eaeac70c50ec998b47734c59a49a13f0023ea
KofiWhoIsHe <kofagiusz@gmail.com>
2025-03-13 15:19:39 +0100

    POPRAWKI MALE TAM

--- client/script.js
+++ client/script.js
@@ -1,1 +1,18 @@
 //fajny pomysl z position fix
+
+let cSub=document.getElementById("WyborLekcji")
+cSub.addEventListener("click",()=>{
+    console.log(cSub.value)
+
+    async function showGrade() {
+        const res= await fetch(`http://localhost:3000/show-grades/${cSub.value}`)
+        const grade= await res.json()
+
+        let dlugosc=grade.length-1
+        while(dlugosc>=0){
+            console.log(grade.grade[dlugosc])
+            dlugosc--;
+        }
+    }
+    showGrade()
+})
--- serwer/index.js
+++ serwer/index.js
@@ -10,15 +10,23 @@ const app=express()
 app.use(cors())
 
 //Laczenie sie z baza danych
-
+var con=mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    password:"",
+    database:"szkoła"
+})
 //Wyswietlanie uzytkownikow (potrzebne do logowania)
 app.get("/show-users",(req,res)=>{
-    res.send("Narazie tyle ci pokarze essa")
+    res.send("Narazie tyle ci pokaże essa")
 })
 //Wyswietlanie ocen
-app.get("/show-grades/:uczenID",(req,res)=>{
-    let id=req.params.uczenID
-    res.send("tobie rowniez narazie nic nie pokarze ale ocenki dla ucznia o ID "+id+ " To id to jest: "+typeof(id))
+app.get("/show-grades/:subject",(req,res)=>{
+    let subject=req.params.subject
+    sql=`Select grade from grades where subject like '${subject}'`
+    con.query(sql,(err,wynik0)=>{
+        res.json(wynik0)
+    })
 })
 //Dodawanie ocen
 app.get("/add-grade/:przedmiot/:ocena/:uczenId",(req,res)=>{
@@ -27,7 +35,6 @@ app.get("/add-grade/:przedmiot/:ocena/:uczenId",(req,res)=>{
     let przedmiot=req.params.przedmiot
     res.send("dodano ocene: "+ocena+" z "+przedmiot+" dla ucznia o id: "+id)
 })
-//
 //
 app.listen(port,()=>{
     console.log("Aplikacja uruchomiona")
